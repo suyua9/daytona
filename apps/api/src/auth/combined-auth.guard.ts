8fe61d0533b8c89d3d718ef2d81d179e6eb63c02
Ivan Dagelic <dagelic.ivan@gmail.com>
2026-04-01 08:31:05 +0000

    fix(api): improve api responses on 401
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- apps/api/src/auth/combined-auth.guard.ts
+++ apps/api/src/auth/combined-auth.guard.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0
  */
 
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common'
+import { Injectable, HttpException, Logger, UnauthorizedException } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 /**
@@ -20,7 +20,15 @@ export class CombinedAuthGuard extends AuthGuard(['api-key', 'jwt']) {
   private readonly logger = new Logger(CombinedAuthGuard.name)
 
   handleRequest(err: any, user: any) {
-    if (err || !user) {
+    if (err) {
+      if (err instanceof HttpException && err.getStatus() >= 500) {
+        throw err
+      }
+      this.logger.debug('Authentication failed', { err, user })
+      throw new UnauthorizedException('Invalid credentials')
+    }
+
+    if (!user) {
       this.logger.debug('Authentication failed', { err, user })
       throw new UnauthorizedException('Invalid credentials')
     }
